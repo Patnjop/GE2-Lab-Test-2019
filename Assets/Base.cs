e1e61fd31ab7da225b63cd3c4aaf5acb389e46f1
Patnjop <36369847+Patnjop@users.noreply.github.com>
2019-03-26 10:05:45 +0000

    Fighter Spawning and base accumulating tiberium

--- Assets/Base.cs
+++ Assets/Base.cs
@@ -11,6 +11,7 @@ public class Base : MonoBehaviour
 
     public GameObject fighterPrefab;
 
+    bool tiberiumLoaded;
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +25,31 @@ public class Base : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (tiberiumLoaded == false)
+        {
+            StartCoroutine("tiberiumTimer");
+            tiberiumLoaded = true;
+        }
         text.text = "" + tiberium;
+    }
+
+    IEnumerator tiberiumTimer()
+    {
+        yield return new WaitForSeconds(1f);
+        if (tiberium >= 10)
+        {
+            GameObject fighter = GameObject.Instantiate(fighterPrefab, this.transform.position, Quaternion.identity);
+            fighter.transform.SetParent(this.transform);
+            foreach (Renderer q in GetComponentsInChildren<Renderer>())
+            {
+                q.material.color = this.GetComponent<Renderer>().material.color;
+            }
+            tiberium -= 7;
+        }
+        else
+        {
+            tiberium++;
+        }
+        tiberiumLoaded = false;
     }
 }
